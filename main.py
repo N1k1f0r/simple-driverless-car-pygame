8abfb4fce47c4eec9ad0957fbd5c87b3ea54aa9a
N1k1f0r <rzewecki@gmail.com>
2026-03-04 19:37:35 +0100

    Finished CarEnv class. Random steer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
 import gymnasium as gym
+import numpy as np
 
 import pygame
 #Kolory
@@ -39,87 +40,95 @@ class CarEnv(gym.Env):
         self.angle=0
         self.speed=0
 
+        observation = self._get_obs()
+        return observation, {}
+
     def isOnTrack(self, x, y):
         if 0 <= x < WIDTH and 0 <= y < HEIGHT:
             if self.track_surface.get_at((int(x), int(y)))[1] < 50:
                 return True
         return False
 
-    def get_radar_distance(self,x, y, angle_rad, accurcy=1, max_steps=100):
-        def getLength(x, y, x1, y1):
-            l = (x - x1) ** 2 + (y - y1) ** 2
-            return math.sqrt(l)
-        dx = math.cos(angle_rad) * accurcy
-        dy = math.sin(angle_rad) * accurcy
-        x1 = x + dx
-        y1 = y + dy
+    def getLength(self, x1, y1):
+        l = (self.x - x1) ** 2 + (self.y - y1) ** 2
+        return math.sqrt(l)
+
+    def get_radar_distance(self, angle_rad, accuracy=1, max_steps=300):
+        dx = math.cos(angle_rad) * accuracy
+        dy = math.sin(angle_rad) * accuracy
+        x1 = self.x + dx
+        y1 = self.y + dy
         steps = 0
         color = YELLOW
         while self.isOnTrack(x1, y1) and steps < max_steps:
             x1 += dx
             y1 += dy
             steps+=1
-        l = getLength(x, y, x1, y1)
+        l = self.getLength(x1, y1)
         if l < 40:
             color = RED
-        pygame.draw.line(self.screen, color, (x, y), (x1, y1), 2)
-        return getLength(x, y, x1, y1)
+        pygame.draw.line(self.screen, color, (self.x, self.y), (x1, y1), 2)
+        return self.getLength(x1, y1)
+    def _get_obs(self):
+        rad=math.radians(self.angle)
+        return np.array([
+            self.get_radar_distance(rad-offset),
+            self.get_radar_distance(rad),
+            self.get_radar_distance(rad+offset)
+        ],dtype=np.float32)
+    def step(self, action):
+        throttle,steer=action
+        if throttle==1:
+            self.speed=min(self.speed+0.1,max_speed)
+        else:
+            self.speed*=0.95
 
+        if steer==0:
+            self.angle-=rotation_speed
+        elif steer==2:
+            self.angle+=rotation_speed
 
+        rad=math.radians(self.angle)
+        self.x+=self.speed*math.cos(rad)
+        self.y+=self.speed*math.sin(rad)
 
+        terminated=False
+        reward=0.1
 
+        if not self.isOnTrack(self.x,self.y):
+            terminated=True
+            reward=-100
+        else:
+            reward+=self.speed*0.2
 
+        observation = self._get_obs()
+        return observation,reward,terminated,False,{}
 
+    def render(self):
+        self.screen.blit(self.track_surface,(0,0))
+        car_surf = pygame.Surface((car_width, car_height), pygame.SRCALPHA)
+        pygame.draw.rect(car_surf, CAR_COLOR, (0, 0, 50, 30))
+        rotated_car = pygame.transform.rotate(car_surf, -self.angle)
+        rect = rotated_car.get_rect(center=(self.x, self.y))
 
+        self._get_obs()
+        self.screen.blit(rotated_car,rect.topleft)
+        pygame.display.flip()
+        self.clock.tick(60)
 
-
-
-
-
-
-
-
-
-
-running=True
+#Uruchomienie
+env=CarEnv()
+obs,info=env.reset()
+running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    action = env.action_space.sample()
 
+    obs, reward, terminated, truncated, info = env.step(action)
+    env.render()
 
-    screen.blit(track_surface,(0,0))
-    keys=pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        angle-=rotation_speed
-    if keys[pygame.K_RIGHT]:
-        angle+=rotation_speed
-    if keys[pygame.K_UP]:
-        speed=min(speed+0.1,max_speed)
-    elif keys[pygame.K_DOWN]:
-        speed=max(speed-0.1,-max_speed//2)
-    else:
-        speed*=0.95
-    radians=math.radians(angle)
-    x+=speed*math.cos(radians)
-    y+=speed*math.sin(radians)
-
-    #screen.fill(BLACK)
-    if isOnTrack(x,y):
-        car =pygame.Surface((car_width,car_height),pygame.SRCALPHA)
-        pygame.draw.rect(car,CAR_COLOR,(0,0,50,30))
-        rotated_car=pygame.transform.rotate(car,-angle)
-        rect=rotated_car.get_rect(center=(x,y))
-        screen.blit(rotated_car,rect.topleft)
-        print(get_radar_distance(x,y,radians-offset),
-        get_radar_distance(x,y,radians),
-        get_radar_distance(x,y,radians+offset))
-    else:
-        reset()
-
-
-    pygame.display.flip()
-
-    clock.tick(60)  # limits FPS to 60
-
+    if terminated:
+        env.reset()
 pygame.quit()
